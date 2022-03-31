955513a2cd6767cfceae9f6758078fb2ac96a0c3
tjlatehjr <64728882+tjlatehjr@users.noreply.github.com>
2022-03-31 10:44:16 -0400

    First commit

---
+++ Flask Market/models.py
@@ -0,0 +1,9 @@
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=10), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
---
+++ Flask Market/routes.py
@@ -0,0 +1,10 @@
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market/')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
